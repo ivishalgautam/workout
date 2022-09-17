8f98e3ddffc8bed47532df098c4850e4432943c2
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2022-09-17 20:40:29 +0530

    low sized images

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const arrow = document.querySelectorAll("i");
-const workout = document.querySelectorAll(".workout");
+const workouts = document.querySelectorAll(".workout");
+
+// workouts.forEach(item => {})
 
 arrow.forEach((item) => {
   item.style.display = "none";
 });
 
-workout.forEach((item) =>
+workouts.forEach((item) =>
   item.addEventListener("mouseover", function (e) {
     e.target.children[0].children[1].children[0].classList.add("show");
   })
 );
 
-workout.forEach((item) =>
+workouts.forEach((item) =>
   item.addEventListener("mouseout", function (e) {
     e.target.children[0].children[1].children[0].classList.remove("show");
   })
